02ce13fd30b9a07e7e648a45681c4d1c0c9b8815
David Pinder <davejspin@users.noreply.github.com>
2017-03-14 16:48:38 +0000

    Added app.js with routes defined

---
+++ app.js
@@ -0,0 +1,21 @@
+(function() {
+
+  var app = angular.module("githubViewer", ["ngRoute"]);
+
+  app.config(function($routeProvider) {
+    $routeProvider
+      .when("/main", {
+        templateUrl: "main.html",
+        controller: "MainController"
+      })
+      .when("/user/:username", {
+        templateUrl: "userdetails.html",
+        controller: "UserController"
+      })
+      .otherwise({
+        redirectTo: "/main"
+      });
+
+  });
+
+}());
