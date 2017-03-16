f7ecb7501817a5bfa16e9ceca04800984c3d8588
David Pinder <davejspin@users.noreply.github.com>
2017-03-16 20:31:20 +0000

    Simplified github service API
    removed getContributors service

--- github.js
+++ github.js
@@ -1,7 +1,7 @@
 (function() {
 
   var github = function($http) {
-    
+
     var apiUrl = "https://api.github.com";
 
     var getUser = function(username) {
@@ -20,30 +20,28 @@
     };
 
     var getRepoDetails = function(username, reponame) {
-
+      var repo;
       var serviceUrl = apiUrl + "/repos/" + username + "/" + reponame;
-      return $http.get(serviceUrl).then(function(response) {
-        return response.data;
-      });
-      
-    };
-
-    var getContributors = function(repo) {
-      return $http.get(repo.contributors_url).then(function(response) {
-        return response.data;
-      });
+      return $http.get(serviceUrl)
+        .then(function(response) {
+          repo = response.data;
+          return $http.get(serviceUrl + "/contributors");
+        })
+        .then(function(response) {
+          repo.contributors = response.data;
+          return repo;
+        });
     };
 
     return {
       getUser: getUser,
       getRepos: getRepos,
-      getRepoDetails: getRepoDetails,
-      getContributors: getContributors
+      getRepoDetails: getRepoDetails
     };
 
   };
 
   var module = angular.module("githubViewer");
   module.factory("github", github);
-  
+
 }());
